10e9117178ee43192fbf820e1cfe694281974d5f
hiesa1 <hiesa1@naver.com>
2023-07-10 09:51:22 +0900

    파일정리

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [movies, setMovies] = useState([
+    {
+      Title: "The Amazing Spider-Man",
+      Year: "2012",
+      imdbID: "tt0948470",
+      Type: "movie",
+      Poster: "https://m.media-amazon.com/images/M/MV5BMjMyOTM4MDMxNV5BMl5BanBnXkFtZTcwNjIyNzExOA@@._V1_SX300.jpg"
+    },
+    {
+      Title: "The Amazing Spider-Man 2",
+      Year: "2014",
+      imdbID: "tt1872181",
+      Type: "movie",
+      Poster: "https://m.media-amazon.com/images/M/MV5BOTA5NDYxNTg0OV5BMl5BanBnXkFtZTgwODE5NzU1MTE@._V1_SX300.jpg"
+    },
+    {
+      Title: "The Amazing World of Gumball",
+      Year: "2011–2019",
+      imdbID: "tt1942683",
+      Type: "series",
+      Poster: "https://m.media-amazon.com/images/M/MV5BYWU1YTA4OGUtNjcxMC00ZTllLTgxYWUtY2U5NmViZTU0MmNjXkEyXkFqcGdeQXVyMTM0NTUzNDIy._V1_SX300.jpg"
+    }
+  ]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      헬로우
     </div>
   );
 }
